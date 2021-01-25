becd746bd3a5dac2f0c6d3686f7b43028471b709
ayoubkandah <a.kandah994@gmail.com>
2021-01-25 15:24:12 +0200

    fix

--- js/app.js
+++ js/app.js
@@ -55,6 +55,6 @@ btn.onclick = function() {
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
