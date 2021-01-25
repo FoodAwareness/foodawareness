bad5c48f7a557c1c1e21db562d97fd4f979e3156
Motasem odeh <motasem.udeh@gmail.com>
2021-01-25 10:33:23 +0200

    latest updates

--- README.md
+++ README.md
@@ -11,3 +11,20 @@ This website is about health and food awareness, it’ll contain three main sect
 * Any Links you used as reference -->
 
 <!-- ### Reflections and Comments -->
+
+
+### References
+
+#### contact Form
+[Refrence 1](https://www.w3schools.com/howto/howto_css_contact_form.asp)
+
+
+#### background Image
+[Refrence 2](https://www.pexels.com/photo/man-running-on-dirt-road-1465892/)
+
+#### before after css
+[Refrence 3](https://stackoverflow.com/questions/28434521/use-before-after-pseudo-element-to-make-a-line/28434792)
+
+
+#### modal submit contact us
+[Refrence 4](https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal)
--- js/app.js
+++ js/app.js
@@ -24,5 +24,37 @@ function preLoad() {
 
 function showPage() {
   document.getElementById("loader").style.display = "none";
-  document.getElementById("myPage").style.display = "block";
+  // document.getElementById("myPage").style.display = "grid";
+}
+
+
+
+// submit contact us Form
+
+window.onclick = function(event) {
+  event.preventDefault();
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+//Modal
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("sendMessage");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
 }
