0fbbc984ca28b0897460791c1c7921598e5bc2ae
hamzashamoun96 <hamza.shamoun96@gmail.com>
2021-01-25 18:17:14 +0200

    preparing the form

---
+++ js/form.js
@@ -0,0 +1,155 @@
+'use strict'
+
+// Switch between tabs
+
+var tabcontent = document.getElementsByClassName("tabcontent");
+function openForm(evt, formName) {
+  var i, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(formName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+// Select Radio
+
+function switchradio(evt) {
+    var radiolabelMale = document.getElementsByClassName("radio-label-male")
+    var radiolabelFemale = document.getElementsByClassName("radio-label-female")
+    for (var i = 0; i < radiolabelMale.length; i++) {
+        radiolabelMale[i].className = radiolabelMale[i].className.replace(" active", "");
+    }
+    for (var i = 0; i < radiolabelFemale.length; i++) {
+        radiolabelFemale[i].className = radiolabelFemale[i].className.replace(" active", "");
+    }
+    evt.currentTarget.className += " active";
+}
+
+// Decliration And Caculation Functions For BMR , BMI , BFP , Carbohydrates
+
+var Age, Height, Weight, Activity, BMRmale, BMRfemale, BMI, BFPadults, BFPchildren, Carbs40, Carbs65, Carbs75, Protein10, Protein35, Gender;
+function CalculateBMRmale() {
+    BMRmale = Math.floor(((10 * Weight) + (6.25 * Height) - (5 * Age) + 5) * Activity)
+}
+
+function CalculateBMRfemale() {
+    BMRfemale = Math.floor(((10 * Weight) + (6.25 * Height) - (5 * Age) - 161) * Activity)
+}
+
+function CalculateBMI() {
+    BMI = Math.floor(Weight / Math.pow(Height / 100, 2))
+}
+
+function CalculateBFPadults() {
+    BFPadults = Math.floor((1.2 * BMI) + (0.23 * Age) - (10.8 * Gender) - 5.4)
+}
+
+function CalculateBFPchildren() {
+    BFPchildren = Math.floor((1.51 * BMI) - (0.7 * Age) - (3.6 * Gender) + 1.4)
+}
+function CalculateCarbs(BMR) {
+    Carbs40 = Math.floor((BMR * 0.4) / 4)
+    Carbs65 = Math.floor((BMR * 0.65) / 4)
+    Carbs75 = Math.floor((BMR * 0.75) / 4)
+}
+function CalculateProtein(BMR) {
+    Protein10 = Math.floor((BMR * 0.1) / 4)
+    Protein35 = Math.floor((BMR * 0.35) / 4)
+}
+
+// Add Listener For Adults Form
+
+var AdultsForm = document.getElementById('adults-form');
+AdultsForm.addEventListener('submit', Result1);
+function Result1(event1) {
+    event1.preventDefault();
+    //console.log(event1);
+    Age = event1.target.AgeAdults.value;
+    Height = event1.target.Height.value;
+    Weight = event1.target.Weight.value;
+    if (event1.target.male.labels[0].className === 'radio-label-male active') {
+        Gender = 1;
+    } else {
+        Gender = 0;
+    }
+    Activity = event1.target.ActivityAdults.value;
+    CalculateBMI();
+    CalculateBFPadults();
+    if (Gender === 1) {
+        CalculateBMRmale();
+        CalculateCarbs(BMRmale)
+        CalculateProtein(BMRmale)
+    } else {
+        CalculateBMRfemale();
+        CalculateCarbs(BMRfemale)
+        CalculateProtein(BMRfemale)
+    }
+    console.log(BMRmale, BMRfemale, BMI, BFPadults, Carbs40, Carbs65, Carbs75, Gender)
+    LocalStoreAdults()
+}
+
+// Add Listener For Children Form
+
+var ChildrenForm = document.getElementById('children-form');
+ChildrenForm.addEventListener('submit', Result2);
+function Result2(event2) {
+    event2.preventDefault();
+    //console.log(event2);
+    Age = event2.target.AgeChildren.value;
+    Height = event2.target.Height.value;
+    Weight = event2.target.Weight.value;
+    // male = 1 , female = 0;
+    if (event2.target.male.nextElementSibling.className === 'radio-label-male active') {
+        Gender = 1;
+    } else {
+        Gender = 0;
+    }
+    Activity = event2.target.ActivityChildren.value;
+    CalculateBMI();
+    CalculateBFPchildren();
+    if (Gender === 1) {
+        CalculateBMRmale();
+        CalculateCarbs(BMRmale)
+        CalculateProtein(BMRmale)
+    } else {
+        CalculateBMRfemale();
+        CalculateCarbs(BMRfemale)
+        CalculateProtein(BMRfemale)
+    }
+}
+
+function LocalStoreAdults() {
+    localStorage.setItem('Age', Age)
+    localStorage.setItem('Height', Height)
+    localStorage.setItem('Weight', Weight)
+    localStorage.setItem('Gender', Gender)
+    localStorage.setItem('Activity', Activity)
+    localStorage.setItem('BMI', BMI)
+    localStorage.setItem('BFP', BFPadults)
+    if (Gender === 1) {
+        localStorage.setItem('BMR', BMRmale)
+    } else {
+        localStorage.setItem('BMR', BMRfemale)
+    }
+    localStorage.setItem('Carbohydrates 40 %', Carbs40)
+    localStorage.setItem('Carbohydrates 65 %', Carbs65)
+    localStorage.setItem('Carbohydrates 75 %', Carbs75)
+
+    localStorage.setItem('Protein 10%', Protein10)
+    localStorage.setItem('Protein 35%', Protein35)
+}
+
+
+
+/*var ShowResult;
+function render (){
+    ShowResult = document.getElementById('Results')
+
+}*/
